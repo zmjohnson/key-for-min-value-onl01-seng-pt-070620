0df41b6758773896cf77d311ee310981a49ba55f
Zachary Johnson <zacharymjohnson2@gmail.com>
2020-07-17 03:21:27 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,4 +8,11 @@ current_minimum_val = nil
       current_minimum_val = value 
       current_key_assoc_w_min_val = key 
     else 
+         if current_minimum_val > value 
+        current_minimum_val = value
+        current_key_assoc_w_min_val = key 
+      end
+    end
+  end 
+  current_key_assoc_w_min_val 
 end
