b5b5809c0bdf7cea51babd9ca7b5445242187553
Zachary Johnson <zacharymjohnson2@gmail.com>
2020-07-17 03:22:02 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,13 +2,16 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-current_minimum_val = nil 
+
+  current_minimum_val = nil 
   current_key_assoc_w_min_val = nil 
+
     if current_minimum_val == nil 
       current_minimum_val = value 
       current_key_assoc_w_min_val = key 
     else 
-         if current_minimum_val > value 
+
+      if current_minimum_val > value 
         current_minimum_val = value
         current_key_assoc_w_min_val = key 
       end
