bda130598d9090ff2d24f7aa2e9e5515fea6798b
Zachary Johnson <zacharymjohnson2@gmail.com>
2020-07-17 03:23:04 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,18 @@
 
 def key_for_min_value(name_hash)
 
+  current_minimum_val = nil 
+  current_key_assoc_w_min_val = nil 
+  name_hash.each do |key, value|
+    if current_minimum_val == nil 
+      current_minimum_val = value 
+      current_key_assoc_w_min_val = key 
+    else 
+      if current_minimum_val > value 
+        current_minimum_val = value
+        current_key_assoc_w_min_val = key 
+      end
+    end
+  end 
+  current_key_assoc_w_min_val 
 end
